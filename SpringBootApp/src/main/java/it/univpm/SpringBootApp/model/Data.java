6b625d51d765ea7eb61a6ae959ef0131a43a927b
CristianCingo <52571810+CristianCingo@users.noreply.github.com>
2020-05-25 21:48:29 +0200

    // primo commit di prova + classe Data nel model

---
+++ SpringBootApp/src/main/java/it/univpm/SpringBootApp/model/Data.java
@@ -0,0 +1,21 @@
+package it.univpm.SpringBootApp.model;
+
+public class Data {
+	public String id;
+	public boolean can_upload;
+	public int count;
+	public String cover_photo;
+	//ublic String created_time;
+	public String description;
+	public String event;
+	public String from;
+	public String link;
+	public String location;
+	public String name;
+	public String place;
+	public String privacy;
+	//public enum{app, cover, profile, mobile, wall, normal, album} type;
+	//public String updated_time;
+
+
+}

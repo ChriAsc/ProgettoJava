a8386430b88f79fdccfbfe56ef061ecb48b9afc6
CristianCingo <52571810+CristianCingo@users.noreply.github.com>
2020-06-01 19:54:13 +0200

    modifica database, aggiunta campi arraylist<metadata>

--- SpringBootApp/src/main/java/it/univpm/SpringBootApp/model/Database.java
+++ SpringBootApp/src/main/java/it/univpm/SpringBootApp/model/Database.java
@@ -15,25 +15,40 @@ public class Database {
 	private ArrayList<Data> arrData = new ArrayList<Data>();
 	private ArrayList<Metadata> arrMetadata = new ArrayList<Metadata>();
 	
+	
 	/**
 	* Costruttore della classe Database
 	 */
 	
 	public Database()
 	{
-		arrMetadata.add(new Metadata("id", "string"));
+		arrMetadata.add(new Metadata("id", "String"));
 		arrMetadata.add(new Metadata("can_upload", "boolean"));
 		arrMetadata.add(new Metadata("count", "int"));
-		arrMetadata.add(new Metadata("created_time", "string"));
-		arrMetadata.add(new Metadata("description", "string"));
-		arrMetadata.add(new Metadata("event", "string"));
-		arrMetadata.add(new Metadata("link", "string"));
-		arrMetadata.add(new Metadata("location", "string"));
-		arrMetadata.add(new Metadata("name", "string"));
+		arrMetadata.add(new Metadata("created_time", "String"));
+		arrMetadata.add(new Metadata("description", "String"));
+		arrMetadata.add(new Metadata("event", "String"));
+		arrMetadata.add(new Metadata("link", "String"));
+		arrMetadata.add(new Metadata("location", "String"));
+		arrMetadata.add(new Metadata("name", "String"));
+		
 		arrMetadata.add(new Metadata("place", "Place"));
-		arrMetadata.add(new Metadata("privacy", "string"));
-		arrMetadata.add(new Metadata("type", "string"));
+		
+		arrMetadata.add(new Metadata("name_place", "String"));
+		arrMetadata.add(new Metadata("location_place", "Location"));
+		arrMetadata.add(new Metadata("id_place", "string"));
+		
+		arrMetadata.add(new Metadata("city_location", "String"));
+		arrMetadata.add(new Metadata("country_location", "String"));
+		arrMetadata.add(new Metadata("latitude_location", "double"));
+		arrMetadata.add(new Metadata("longitude_location", "double"));
+		arrMetadata.add(new Metadata("zip_location", "String"));
+		
+		arrMetadata.add(new Metadata("privacy", "String"));
+		arrMetadata.add(new Metadata("type", "String"));
 		arrMetadata.add(new Metadata("updated_time", "float"));
+		
+		
 	}
 	
 	/**
